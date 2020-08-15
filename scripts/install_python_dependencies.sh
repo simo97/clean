9559d578c004bfff8149dc1386358a56b3a0e7c8
Adonis SIMO <simoadonis@yahoo.fr>
2020-08-15 15:31:08 +0100

    update venv b ase python version

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 chown ec2-user:ec2-user /home/ec2-user/www
-virtualenv /home/ec2-user/www/project-venv
+virtualenv -p python3.6 /home/ec2-user/www/project-venv
 chown ec2-user:ec2-user /home/ec2-user/www/project-venv
 chown ec2-user:ec2-user /home/ec2-user/www/project-venv/*
 source /home/ec2-user/www/project-venv/bin/activate
